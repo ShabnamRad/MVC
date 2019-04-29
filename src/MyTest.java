8b2f41f0fe321406d28b15ac6f48e87d9a737c5b
Shabnam Ghasemirad <shabnam.996@gmail.com>
2019-04-29 19:51:21 +0430

    travis

---
+++ src/MyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+	@Test
+	public void test1(){
+        assertEquals(1, 1);
+    }
+	
+	@Test
+	public void test2(){
+        assertEquals(2, 2);
+	}
+}
